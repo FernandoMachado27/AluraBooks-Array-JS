5e67d48aac6fec7d29e89d10f98527355dbe2396
FernandoMachado27 <fernando2747@yahoo.com>
2024-01-10 12:12:18 -0300

    Fetch de livros no console

---
+++ app/main.js
@@ -0,0 +1,9 @@
+let livros = []
+const endpointDaApi = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
+getBuscarLivrosDaApi()
+
+async function getBuscarLivrosDaApi() { // requisição para endpoint de livros
+    const resposta = await fetch(endpointDaApi)
+    livros = await resposta.json()
+    console.table(livros);
+}
